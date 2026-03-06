33092006cd72cc42e4de821341e4b1520bea6df3
Danson Nganga <dansonnganga019@gmail.com>
2026-03-07 00:26:22 +0300

    commit

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,27 +5,67 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Enter your marks: ");
-        string marks= Console.ReadLine();
-        int mark= int.Parse(marks);
+        string marks = Console.ReadLine();
+        int mark = int.Parse(marks);
         if (mark >= 90)
         {
-            Console.WriteLine("Grade is A");
+            if (mark < 93)
+            {
+                Console.WriteLine("Grade is -A");
+            }
+            else
+            {
+                Console.WriteLine("Grade is A");
+            }
         }
         else if (mark >= 80)
         {
-            Console.WriteLine("Grade is B");
+            if (mark < 83)
+            {
+                Console.WriteLine("Grade is -B");
+            }
+            else if (mark > 87)
+            {
+                Console.WriteLine("Grade is +B");
+            }
+            else
+            {
+                Console.WriteLine("Grade is B");
+            }
         }
         else if (mark >= 70)
         {
-            Console.WriteLine("Grade is C");
+            if (mark < 73)
+            {
+                Console.WriteLine("Grade is -C");
+            }
+            else if (mark > 77)
+            {
+                Console.WriteLine("Grade is +C");
+            }
+            else
+            {
+                Console.WriteLine("Grade is C");
+            }
         }
         else if (mark >= 60)
         {
-            Console.WriteLine("Grade is D");
+            if (mark < 63)
+            {
+                Console.WriteLine("Grade is -D");
+            }
+            else if (mark > 67)
+            {
+                Console.WriteLine("Grade is +D");
+            }
+            else
+            {
+                Console.WriteLine("Grade is D");
+            }
         }
-        else 
+        else
         {
             Console.WriteLine("Grade is E");
-        }    
+        }
     }
 }
