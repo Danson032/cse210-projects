45660b902f8ee406f9379f15a8a176823553590f
Danson Nganga <dansonnganga019@gmail.com>
2026-03-07 00:13:27 +0300

    commit

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your marks: ");
+        string marks= Console.ReadLine();
+        int mark= int.Parse(marks);
+        if (mark >= 90)
+        {
+            Console.WriteLine("Grade is A");
+        }
+        else if (mark >= 80)
+        {
+            Console.WriteLine("Grade is B");
+        }
+        else if (mark >= 70)
+        {
+            Console.WriteLine("Grade is C");
+        }
+        else if (mark >= 60)
+        {
+            Console.WriteLine("Grade is D");
+        }
+        else 
+        {
+            Console.WriteLine("Grade is E");
+        }    
     }
 }
