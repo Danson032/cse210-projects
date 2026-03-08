e3df62b495cb80107bcefba1170ac1c67faf5af6
Danson Nganga <dansonnganga019@gmail.com>
2026-03-08 16:25:32 +0300

    commit

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job Job1 = new Job();
+        Job1._jobTitle = "Software Engineer";
+        Job1._companyName = "Amazon";
+        Job1._startYear = 2020;
+        Job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._companyName = "Apple";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+
+        Resume myResume = new Resume();
+        myResume._personName = "Danson Nganga";
+        myResume._jobs.Add(Job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
