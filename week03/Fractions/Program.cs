8335764fd68b945d96221c74535f9b0e4c97ed3c
Danson Nganga <dansonnganga019@gmail.com>
2026-03-18 00:48:58 +0300

    commit

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction F1 = new Fraction();
+        Fraction F2 = new Fraction(5);
+        Fraction F3 = new Fraction(3, 4);
+
+        Console.WriteLine(F1.GetFractionString());
+        Console.WriteLine(F2.GetFractionString());
+        Console.WriteLine(F3.GetFractionString());
+
+        F1.SetTop(1);
+        F1.SetBottom(3);
+
+        Console.WriteLine(F1.GetTop());
+        Console.WriteLine(F1.GetBottom());
+        Console.WriteLine(F1.GetFractionString());
+        Console.WriteLine(F1.GetDecimalValue());
     }
 }
